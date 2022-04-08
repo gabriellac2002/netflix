57d7a51136047b60c064ab7b1cee175045adb1c8
Gabi <gabriellacsilva2002@gmail.com>
2022-04-08 18:33:57 -0300

    link entre o app e o tmdb

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Tmdb from './Tmdb';
 
 export default () => {
+
+  useEffect( ()=>{
+    const loadAll = async () => {
+      // pegando a lista total
+    }
+
+    loadAll();
+
+  },[])
+
   return(
     <div>
       Ola mundo
