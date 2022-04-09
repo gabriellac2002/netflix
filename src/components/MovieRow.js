1da643e63ac5bae68f2269ddceeff5ce48162494
Gabi <gabriellacsilva2002@gmail.com>
2022-04-08 22:35:27 -0300

    estilização das listas

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -3,12 +3,19 @@ import './MovieRow.css' ;
 
 export default ({title, items}) =>{
     return(
-        <div>
+        <div className="movieRow">
             <h2> {title} </h2>
             <div className="movieRow--listarea">
-                {items.results.length > 0 && items.results.map((item, key)=>(
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
-                ))}
+
+                <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item, key)=>(
+                        <div key={key} className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
+                        </div>
+                    ))}
+                </div>
+
+                
             </div>
         </div>
     );
