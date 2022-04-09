e65e02996ed4397a3341d0b954878242507154ec
Gabi <gabriellacsilva2002@gmail.com>
2022-04-08 23:00:22 -0300

    sortear um filme para ser o filme em destaque

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
 
   useEffect( ()=>{
     const loadAll = async () => {
       // pegando a lista total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //pegando o filme em destaque
+      let originals = list.filter(i=>i.slug == 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
     }
 
     loadAll();
@@ -20,11 +27,15 @@ export default () => {
 
   return(
     <div className="page">
-        <section className="lists">
-          {movieList.map((item, key)=>(
-            <MovieRow key={key} title={item.title} items={item.items}/>
-          ))}
-        </section>
+
+      {featureData && <FeaturedMovie item={featureData} />}
+      
+
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
   );
 }
