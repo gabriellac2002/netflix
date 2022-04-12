53a117e23764d81f920f5af53b00d04060e8adce
Gabi <gabriellacsilva2002@gmail.com>
2022-04-12 18:22:53 -0300

    imagem de fundo

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,12 +3,13 @@ import "./FeaturedMovie.css";
 
 export default ({item})=> {
     return(
-        <section className="featured" style={{
+        <div className="featured" style={{
             backgroudSize: 'cover',
             backgroudPosition: 'center',
-            backgroudImage:`url(https://image.tmdb.org/t/p/original/${item.backdrop_path})` 
+            backgroudImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})` 
         }}>
+            <img src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`} />
             <div> {item.original_name} </div>
-        </section>
+        </div>
     );
 }
